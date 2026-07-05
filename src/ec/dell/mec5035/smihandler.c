Amersel/coreboot

ec/dell/mec5035: Add S3 suspend SMI handler
This is necessary for S3 resume to work on SNB and newer Dell Latitude
laptops. If a command isn't sent, the EC cuts power to the DIMMs,
preventing the system from resuming. These commands were found using an
FPGA to log all LPC bus transactions between the host and the EC and
then narrowing down which ones were actually necessary.

Interestingly, the command IDs appear to be identical to those in
ec/google/wilco, the EC used on Dell Latitude Chromebooks, and that EC
implements a similar S3 SMI handler as the one implemented in this
commit. The Wilco EC Kconfig does suggest that its firmware is a
modified version of Dell's usual Latitude EC firmware, so the
similarities seem to be intentional.

These similarities also identified a command to enable or disable wake
sources like the power button and lid switch, and this was added to the
SMI handler to disable lid wake as the system does not yet resume
properly from a like wake with coreboot.

Tested on the Latitude E6430 (Ivy Bridge) and the Precision M6800
(Haswell, not yet pushed).

Change-Id: I655868aba46911d128f6c24f410dc6fdf83f3070
Signed-off-by: Nicholas Chin <nic.c3.14@gmail.com>

src/ec/dell/mec5035/smihandler.c
@@ -1,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <acpi/acpi.h>
+#include <console/console.h>
+#include <ec/acpi/ec.h>
+#include "mec5035.h"
+
+void mec5035_sleep(int slp_type)
+{
+	switch (slp_type) {
+	case ACPI_S3:
+		/* System does not yet resume properly if woken by lid */
+		mec5035_change_wake(EC_ACPI_WAKE_LID, WAKE_OFF);
+		mec5035_sleep_enable();
+		break;
+	}
+}

nikolwasalreadyinuse/Latitude-3470-Coreboot

src/ec/dell/mec5035/smihandler.c
@@ -1,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <acpi/acpi.h>
+#include <console/console.h>
+#include <ec/acpi/ec.h>
+#include "mec5035.h"
+
+void mec5035_smi_sleep(int slp_type)
+{
+	switch (slp_type) {
+	case ACPI_S3:
+		/* System does not yet resume properly if woken by lid */
+		mec5035_change_wake(EC_ACPI_WAKE_LID, WAKE_OFF);
+		mec5035_sleep_enable();
+		break;
+	}
+}